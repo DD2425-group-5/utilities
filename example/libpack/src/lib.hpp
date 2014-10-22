a78cc6d4b535230222acd2dd0dca4cd7e9e97ea6
Michal Staniaszek <m.staniaszek@gmail.com>
2014-10-22 16:59:59 +0200

    Removed example package

--- example/libpack/src/lib.hpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-class Library {
-public:
-  static void libfunc();
-  static int intfunc();
-  static void testfunc();
-};
--- example/libpack/src/tt.cpp
+++
@@ -1,5 +0,0 @@
-#include "tt.hpp"
-
-void tt::print(){
-  std::cout << "tt out2" << std::endl;
-}
