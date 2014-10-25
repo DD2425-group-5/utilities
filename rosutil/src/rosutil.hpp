26c39c209010b0688b20bbe23f8b9a33a72ce090
Michal Staniaszek <m.staniaszek@gmail.com>
2014-10-25 22:04:51 +0200

    Added getParam function for vector params to rosutil

--- rosutil/src/rosutil.hpp
+++ rosutil/src/rosutil.hpp
@@ -1,14 +1,18 @@
 #include <ros/ros.h>
 
-class ROSUtil {
-private:
-    ROSUtil(){}
-public:
+namespace ROSUtil{
     template<typename T>
-    static void getParamGeneric(ros::NodeHandle handle, std::string paramName, T &paramVar);
-    static void getParam(ros::NodeHandle handle, std::string paramName, int &paramVar);
-    static void getParam(ros::NodeHandle handle, std::string paramName, double &paramVar);
-    static void getParam(ros::NodeHandle handle, std::string paramName, std::string &paramVar);
-    static void getParam(ros::NodeHandle handle, std::string paramName, float &paramVar);
-    static void getParam(ros::NodeHandle handle, std::string paramName, bool &paramVar);
+    void getParamGeneric(ros::NodeHandle handle, std::string paramName, T &paramVar);
+    void getParam(ros::NodeHandle handle, std::string paramName, int &paramVar);
+    void getParam(ros::NodeHandle handle, std::string paramName, double &paramVar);
+    void getParam(ros::NodeHandle handle, std::string paramName, std::string &paramVar);
+    void getParam(ros::NodeHandle handle, std::string paramName, float &paramVar);
+    void getParam(ros::NodeHandle handle, std::string paramName, bool &paramVar);
+    template<typename T>
+    void getParamGenericVec(ros::NodeHandle handle, std::string paramName, std::vector<T> &paramVar);
+    void getParam(ros::NodeHandle handle, std::string paramName, std::vector<std::string> &paramVar);
+    void getParam(ros::NodeHandle handle, std::string paramName, std::vector<bool> &paramVar);
+    void getParam(ros::NodeHandle handle, std::string paramName, std::vector<int> &paramVar);
+    void getParam(ros::NodeHandle handle, std::string paramName, std::vector<float> &paramVar);
+    void getParam(ros::NodeHandle handle, std::string paramName, std::vector<double> &paramVar);
 };
